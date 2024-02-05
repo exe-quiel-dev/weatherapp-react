a4bba2a41869f10c445cdb02085f3f57502bb836
exe-quiel-dev <exe.quiel.developer@outlook.com>
2024-02-05 07:14:36 -0300

    Iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+// import axios from 'axios'
+
+// axios.get(`https://api.openweathermap.org/data/2.5/weather?q=buenosaires&appid=86f22c7adcb7ae17839faee72c2822df`)
+
+function App() {
+
+  return (
+    <div>
+      <div className='container mx-auto'>
+        {/* TOP */}
+        <div> 
+
+        </div>
+        {/* BOTTOM */}
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['index.html', './src/**/*.jsx', './pages/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
